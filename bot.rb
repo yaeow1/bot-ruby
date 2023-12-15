b7ba66129490fcf9fd7494835168a8787d1cc601
m3rcy <75048718+yaeow1@users.noreply.github.com>
2023-12-15 17:54:46 -0300

    Add files via upload

--- bot.rb
+++ bot.rb
@@ -1,10 +1,30 @@
 require 'discordrb'
+require 'discordrb/webhooks'
+require 'dotenv/load'
 
-bot = Discordrb::Bot.new token: 'MTE4NDI2NzU2ODE5NDc4MTIxNA.GhPuv3.PTwU1OStxdUogPymXsMM4XdKvpHINKKa9kJsFE', client_id:
-1184267568194781214
+Dotenv.load
+
+WEBHOOK_URL = "https://discord.com/api/webhooks/1184276093876834325/96VOZ2ysb_TDsEDq6kH-tTi2r-uzixmDPZP_p3Ssb-ibSUEjim0FcTM6SBhpZU5bTbYu"
+
+bot = Discordrb::Bot.new token: ENV['TOKEN'], client_id:
+ENV['CLIENT_ID']
+
+client = Discordrb::Webhooks::Client.new(url: WEBHOOK_URL)
+client.execute do |builder|
+      builder.content = "Lol! I've been disconnected"
+      builder.add_embed do |embed|
+      embed.title = "I'm online now sir"
+      embed.timestamp = Time.now
+    end
+end
 
 bot.message(with_text: 'Ping!') do |event|
       event.respond 'Pong!'
+end
+
+
+bot.message(with_text: 'Time') do |event|
+      event.respond "The time now: " 
 end
 
 bot.run
